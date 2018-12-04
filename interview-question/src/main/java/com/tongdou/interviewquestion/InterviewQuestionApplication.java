be9830c8960886c4e16d5b4a0ae2dcae6f707f32
shenyuzhu1 <shenyuzhu@jd.com>
2018-12-04 08:48:08 +0800

    面试题

---
+++ interview-question/src/main/java/com/tongdou/interviewquestion/InterviewQuestionApplication.java
@@ -0,0 +1,12 @@
+package com.tongdou.interviewquestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewQuestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewQuestionApplication.class, args);
+    }
+}
---
+++ interview-question/src/test/java/com/tongdou/interviewquestion/InterviewQuestionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tongdou.interviewquestion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InterviewQuestionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
